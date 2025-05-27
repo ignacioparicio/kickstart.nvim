f724f996c48e084c6c446fc62caf95627f630f07
Ignacio Paricio <ignacio.paricio@pxl-vision.com>
2025-05-27 08:39:41 +0200

    Fix image opening with `eog`

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,36 +1,43 @@
-local builtin = require('telescope.builtin')
+local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', require('telescope.builtin').live_grep, {})
 
 local function is_image(filepath)
-    local image_extensions = { 'png', 'jpg', 'jpeg', 'bmp', 'gif', 'tiff' }
-    local split_path = vim.split(filepath:lower(), '.', { plain = true })
-    local extension = split_path[#split_path]
-    return vim.tbl_contains(image_extensions, extension)
+  if type(filepath) ~= 'string' then
+    return false
+  end
+  local image_extensions = { 'png', 'jpg', 'jpeg', 'bmp', 'gif', 'tiff' }
+  local split_path = vim.split(filepath:lower(), '.', { plain = true })
+  local extension = split_path[#split_path]
+  return vim.tbl_contains(image_extensions, extension)
 end
 
 _G.open_with_eog = function(filepath)
-    print("Attempting to open: " .. filepath)
-    os.execute('eog ' .. filepath .. ' &')
+  if type(filepath) ~= 'string' then
+    print 'Invalid filepath'
+    return
+  end
+  print('Attempting to open: ' .. filepath)
+  os.execute('eog ' .. filepath .. ' &')
 end
 
-local actions = require("telescope.actions")
+local actions = require 'telescope.actions'
 require('telescope').setup {
-    defaults = {
-        mappings = {
-            i = {
-                ["<CR>"] = function(prompt_bufnr)
-                    local entry = require("telescope.actions.state").get_selected_entry()
-                    local filepath = entry and entry.value or nil
-                    if filepath and is_image(filepath) then
-                        actions.close(prompt_bufnr)
-                        _G.open_with_eog(filepath)
-                    else
-                        actions.select_default(prompt_bufnr)
-                    end
-                end
-            },
-        },
+  defaults = {
+    mappings = {
+      i = {
+        ['<CR>'] = function(prompt_bufnr)
+          local entry = require('telescope.actions.state').get_selected_entry()
+          local filepath = entry and entry.value or nil
+          if filepath and is_image(filepath) then
+            actions.close(prompt_bufnr)
+            _G.open_with_eog(filepath)
+          else
+            actions.select_default(prompt_bufnr)
+          end
+        end,
+      },
     },
+  },
 }
