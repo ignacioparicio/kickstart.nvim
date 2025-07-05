3af02e11789436212eea263fb195d5b38481ff4d
Ignacio Paricio <ignacio.paricio@pxl-vision.com>
2025-07-05 21:59:42 +0200

    Format

--- init.lua
+++ init.lua
@@ -934,15 +934,15 @@ require('lazy').setup({
     'ojroques/nvim-osc52',
     config = function()
       require('osc52').setup {
-        max_length = 0,       -- no limit
-        silent = false,       -- show message on successful copy
-        trim = false,         -- do not trim whitespace
+        max_length = 0, -- no limit
+        silent = false, -- show message on successful copy
+        trim = false, -- do not trim whitespace
       }
 
       -- Auto copy on yank
       local function copy()
         if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
-          require('osc52').copy_register('')
+          require('osc52').copy_register ''
         end
       end
       vim.api.nvim_create_autocmd('TextYankPost', { callback = copy })
