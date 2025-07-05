d43412172435c528029149297c84087a72da7e80
Ignacio Paricio <ignacio.paricio@pxl-vision.com>
2025-07-05 22:00:11 +0200

    Add `<leader>sa` for Telescope `git status`

--- init.lua
+++ init.lua
@@ -438,6 +438,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sm', ':Telescope media_files<CR>', { desc = '[S]earch [M]edia Files' })
+      vim.keymap.set('n', '<leader>sa', builtin.git_status, { desc = '[S]earch Git [A]status' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
